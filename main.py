ae760b5ec7fd73aba1cb18109e2c8ea95cf9cd4b
LuisM <muislarques@gmail.com>
2025-08-25 18:02:09 +0100

    updates: trying to implement chromedriver and selenium to extract html

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+import requests
+
 import webbrowser as wb
 from urllib.parse import urlparse
-import requests
-from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 print(r"""
     ___       ___       ___       ___       ___       ___       ___   
@@ -19,19 +23,20 @@ print("\n*********************************************************************")
 
 # all valid links start with https://www.linkedin.com/jobs/
 validLink = "https://www.linkedin.com/jobs/"
+driver = webdriver.Chrome()
 
 while True:
   # 1. get a valid LinkedIn link
   while True:
-    linkedInLink = input("\nPlease enter a valid LinkedIn job Listing you want to scan:\n->")
+    mainLink = input("\nPlease enter a valid LinkedIn job Listing you want to scan:\n->")
     try:
-      parsedLink = urlparse(linkedInLink)
+      parsedLink = urlparse(mainLink)
       
       # force exception invalid format
       if not (parsedLink.scheme and parsedLink.netloc):
         raise ValueError("Invalid URL format")
       # force exception doesnt have the right format
-      if not linkedInLink.startswith(validLink):
+      if not mainLink.startswith(validLink):
         raise ValueError("Invalid LinkedIn job listing link")
       
       print("\nYou entered a valid job listing link\nOpening browser and extracting information")
@@ -41,7 +46,26 @@ while True:
       print("You entered an invalid job listing link")
 
   # open if link passes validation
-  wb.open(linkedInLink)
+  wb.open(mainLink)
+
+  # 2. fetch html content
+  response = requests.get(mainLink, headers = {"User-Agent": "Mozilla/5.0"})
+  if response.status_code != 200:
+    print(f"Failed to fetch page. Status code: {response.status_code}")
+  else:
+    print(f"Page fetched Successfuly. Status code: {response.status_code}")
+    
+    driver.get(mainLink)
+    time.sleep(5)
+    # find divs
+    divs = driver.find_elements(By.CLASS_NAME, "application-outlet")
+
+    if not divs:
+      print("No div found")
+    else:
+      print("DIV FOUND")
+    
+    driver.quit()
 
   while True:
     pass
